0ab7a9d998b61efa0af1c7328887a39fd4747b65
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-07-08 17:46:08 +0530

    fix: "minor fixes for android and added permission in android manifest"

--- android/app/src/main/kotlin/com/santosh/crewmeister/crewmeister/MainActivity.kt
+++ android/app/src/main/kotlin/com/santosh/crewmeister/crewmeister/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.santosh.crewmeister.crewmeister
+package com.example.ecommerce_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
