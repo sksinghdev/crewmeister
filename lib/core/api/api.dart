dadb1f487b559c247bdd5110e7c42b7d10e71cce
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-07-09 23:10:31 +0530

    fix: "added api class class in core"

---
+++ lib/core/api/api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+const absencesPath = 'assets/json_files/absences.json';
+const membersPath = 'assets/json_files/members.json';
+
+Future<List<dynamic>> readJsonFile(String path) async {
+  String content = await rootBundle.loadString(path);
+  Map<String, dynamic> data = jsonDecode(content);
+  return data['payload'];
+}
+
+Future<List<dynamic>> absences() async {
+  return await readJsonFile(absencesPath);
+}
+
+Future<List<dynamic>> members() async {
+  return await readJsonFile(membersPath);
+}
