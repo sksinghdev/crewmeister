925e79f00949291ff72a9be22e959614216a6d1d
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-07-09 23:15:26 +0530

    fix: "added injection and navigation"

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,39 @@
 import 'package:flutter/material.dart';
 
-import 'core/api/api.dart';
+import 'core/shared/app_router.dart';
+import 'core/shared/injection_container.dart' as di;
 
-
+final _appRouter = AppRouter();
 void main() async{
   WidgetsFlutterBinding.ensureInitialized(); 
-  final result = await absences();
-  print(result);
+  _registerDependencies();
   runApp(const MyApp());
+}
+Future<void> _registerDependencies() async {
+  await di.init();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-         
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-   
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-         
-        title: Text(widget.title),
-      ),
-      body: Center(
-         
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    try {
+      return MaterialApp.router(
+        routerConfig: _appRouter.config(),
+        debugShowCheckedModeBanner: false,
+        title: 'Crewmeister',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
+    } catch (e, stack) {
+      debugPrint('Error during router initialization: $e\n$stack');
+      return const MaterialApp(
+          home:
+              Scaffold(body: Center(child: Text('App initialization failed'))));
+    }
   }
 }
