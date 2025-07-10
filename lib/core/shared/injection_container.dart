9e38793ba76e181e2c38b872ce65e1afe1cc28ca
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-07-10 23:34:12 +0530

    fix: "added injection for usecases and cubit injection fixes"

--- lib/core/shared/injection_container.dart
+++ lib/core/shared/injection_container.dart
@@ -1,16 +1,27 @@
 import 'package:get_it/get_it.dart';
 import '../../data/repositories/absence_repository_impl.dart';
  import '../../domain/repositories/absence_repository.dart';
+import '../../domain/usecases/filter_absences.dart';
+import '../../domain/usecases/get_absence_count.dart';
+import '../../domain/usecases/get_absences.dart';
 import '../../presentation/cubit/absence_cubit.dart';
  
 final sl = GetIt.instance;
 
 Future<void> init() async {
   // Cubit
-  sl.registerFactory(() => AbsenceCubit(sl()));
-
+ 
   // Repository
   sl.registerLazySingleton<AbsenceRepository>(() => AbsenceRepositoryImpl());
+
+  sl.registerLazySingleton(() => GetAbsencesUseCase(sl()));
+sl.registerLazySingleton(() => GetAbsenceCountUseCase(sl()));
+sl.registerLazySingleton(() => FilterAbsencesUseCase(sl()));
+sl.registerFactory(() => AbsenceCubit(
+  getAbsencesUseCase: sl(),
+  getAbsenceCountUseCase: sl(),
+  filterAbsencesUseCase: sl(),
+));
 
 
 }
